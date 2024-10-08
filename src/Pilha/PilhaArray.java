29b15f6fc8790ce69b59e06e21ade7c595d69284
ronaldoMarin <marinhoronaldo02@gmail.com>
2024-10-08 14:04:44 -0300

    chore: first release stack

---
+++ src/Pilha/PilhaArray.java
@@ -0,0 +1,34 @@
+package Pilha;
+import Pilha.PilhaVaziaExcecao;
+
+public class PilhaArray implements Pilha{
+    private int tamanhoPilha;
+    private Object[] lista;
+    private int topo = -1;
+
+    public PilhaArray(int capacidade){
+        this.tamanhoPilha = capacidade;
+        this.lista = new Object[this.tamanhoPilha];
+    }
+
+    //Retorna true or false if top
+    @Override
+    public boolean isEmpty() {
+        return this.topo == -1;
+    }
+
+    //Return sum top + 1
+    @Override
+    public int size() {
+        return this.topo + 1;
+    }
+
+    //Verify
+    @Override
+    public Object top(){
+        if(this.isEmpty()){
+            throw new PilhaVaziaExcecao("A pilha está vazia!");
+        }
+        return this.lista[this.topo];
+    }
+}
